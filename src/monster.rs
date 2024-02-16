f849452e065846539d4849c976ff3a1415e6f2b1
Habu <habu1010+github@gmail.com>
2024-02-16 22:26:48 +0900

    refactor: MonsterRaceDefinitionの生成処理
    format!で一時的に作成したStringオブジェクトを最終的なStringオブジェクトに
    追加していくのではなく、writeln!で最終的なStringオブジェクトに直接追加
    する。

--- src/monster.rs
+++ src/monster.rs
@@ -1,6 +1,6 @@
 mod flags;
 
-use std::collections::BTreeSet;
+use std::{collections::BTreeSet, fmt::Write};
 
 pub use flags::*;
 
@@ -144,22 +144,32 @@ impl MonsterRace {
     pub fn to_monster_race_definition(&self) -> String {
         let mut result = String::new();
 
-        result += &format!("N:XXX:{}\n", self.name);
-        result += &format!("E:{}\n", self.english_name);
-        result += &format!("G:{}:{}\n", self.symbol.char, self.symbol.color.to_char());
-        result += &format!(
-            "I:{}:{}d{}:{}:{}:{}\n",
+        writeln!(result, "N:XXX:{}", self.name).unwrap();
+        writeln!(result, "E:{}", self.english_name).unwrap();
+        writeln!(
+            result,
+            "G:{}:{}",
+            self.symbol.char,
+            self.symbol.color.to_char()
+        )
+        .unwrap();
+        writeln!(
+            result,
+            "I:{}:{}d{}:{}:{}:{}",
             self.speed + 110,
             self.hp.num,
             self.hp.sides,
             self.vision,
             self.ac,
             self.alertness
-        );
-        result += &format!(
-            "W:{}:{}:{}:{}:{}\n",
+        )
+        .unwrap();
+        writeln!(
+            result,
+            "W:{}:{}:{}:{}:{}",
             self.level, self.rarity, self.exp, self.evolving_exp, self.evolves_to
-        );
+        )
+        .unwrap();
         for blow in self.blows.iter() {
             result += &make_blow_line(blow);
         }
@@ -175,7 +185,7 @@ impl MonsterRace {
         skill_flags.extend(self.skill.summons.iter().map(|f| f.token()));
         skill_flags.extend(self.skill.miscs.iter().map(|f| f.token()));
         if !skill_flags.is_empty() {
-            result += &format!("S:1_IN_{}\n", self.skill_use_prob_div);
+            writeln!(result, "S:1_IN_{}", self.skill_use_prob_div).unwrap();
             result += make_flag_lines("S", &skill_flags).as_str();
         }
 
@@ -194,10 +204,11 @@ fn make_blow_line(blow: &MonsterBlow) -> String {
         return result;
     }
 
-    result += &format!("B:{}:{}", blow.method.token(), blow.effect.token(),);
+    write!(result, "B:{}:{}", blow.method.token(), blow.effect.token()).unwrap();
 
     if blow.has_damage {
-        result += &format!(":{}d{}", blow.damage_dice.num, blow.damage_dice.sides);
+        let MonsterHitDice { num, sides } = blow.damage_dice;
+        write!(result, ":{num}d{sides}").unwrap();
     }
 
     result.push('\n');
@@ -219,9 +230,9 @@ fn make_flag_lines(header: &str, token_list: &[&str]) -> String {
 
         let current_line = lines.last_mut().unwrap();
         if current_line.is_empty() {
-            current_line.push_str(&format!("{header}:{token}"));
+            write!(current_line, "{header}:{token}").unwrap();
         } else {
-            current_line.push_str(&format!(" | {token}"));
+            write!(current_line, " | {token}").unwrap();
         }
     }
 
@@ -242,7 +253,7 @@ fn make_flavor_lines(header: &str, flavor: &str) -> String {
         .map(str::trim)
         .filter(|line| !line.is_empty())
     {
-        lines.push_str(&format!("{}{}\n", header, line));
+        writeln!(lines, "{header}{line}").unwrap();
     }
 
     lines
@@ -252,10 +263,12 @@ fn make_escorts_lines(escort_num: usize, escorts: &[MonsterEscort]) -> String {
     let mut result = String::new();
 
     for escort in escorts.iter().take(escort_num) {
-        result += &format!(
+        writeln!(
+            result,
             "R:{}:{}d{}\n",
             escort.monster_id, escort.num.num, escort.num.sides
-        );
+        )
+        .unwrap();
     }
 
     result
